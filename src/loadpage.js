a9008f401ad021318a7101a82adcaf040c28bc0c
John <134107082+jksnook@users.noreply.github.com>
2023-09-25 12:03:20 -0700

    load page with javascript

---
+++ src/loadpage.js
@@ -0,0 +1,32 @@
+export default function loadPage() {
+  const content = document.querySelector('#content');
+  const header = document.createElement('div');
+  header.classList.add('header');
+  const title = document.createElement('h1');
+  title.textContent = "Joe's Pizzeria";
+  const nav = document.createElement('ul');
+  nav.classList.add('nav');
+  const navItem1 = document.createElement('li');
+  navItem1.classList.add('current-tab')
+  navItem1.textContent = 'Home';
+  nav.appendChild(navItem1);
+  const navItem2 = document.createElement('li');
+  navItem2.textContent = 'Menu';
+  nav.appendChild(navItem2);
+  const navItem3 = document.createElement('li');
+  navItem3.textContent = 'Contact';
+  nav.appendChild(navItem3);
+  header.appendChild(title);
+  header.appendChild(nav);
+  content.appendChild(header);
+  const page = document.createElement('div');
+  page.classList.add('page');
+  const imageContainer = document.createElement('div');
+  imageContainer.classList.add('image');
+  const pizza = document.createElement('img');
+  pizza.src = './pizza.jpg';
+  pizza.alt = 'A pizza pie';
+  imageContainer.appendChild(pizza);
+  page.appendChild(imageContainer);
+  content.appendChild(page);
+}
